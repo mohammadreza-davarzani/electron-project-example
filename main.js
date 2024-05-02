1af7ea01276e67096b71524862c5933afc18f2de
mo.davarzani <mohammaddavarzani1998@gmail.com>
2024-05-02 12:38:39 +0330

    feat: add isDev for developer mood

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const {app , BrowserWindow } = require('electron');
 const  path  = require('path');
+const isDev = process.env.NODE_ENV !== "development";
 const isMac = process.platform === 'darwin';
 function createMainWindow(){
     const mainWidow = new BrowserWindow({
         titleL:"image resize",
-        width:1500,
+        width:isDev? 1500 : 500,
         height:1600,
     });
+    if(isDev){
+        mainWidow.webContents,openDevTools();
+    }
     mainWidow.loadFile(path.join(__dirname, "./renderer/index.html"))
 }
 app.whenReady().then(()=>{
